62df3ec14a2966e89a3d6d3354ebb1655e245b39
faizan-abbas-mir <faixanmir21.fm@gmail.com>
2026-01-24 14:36:06 +0530

    feat: add task class and task manager class

--- time-tracker/models/task.py
+++ time-tracker/models/task.py
@@ -1,19 +1,53 @@
 "this file contains the Task model for the time-tracker application"
 from database.connection import get_connection
-con, cursor = get_connection()
+con = get_connection()
+from datetime import datetime
 
 class Tasks:
-    def __init__(self,id,taskname,taskdescription,starttime,endtime,status):
-        self.id=id
+    def __init__(self,user_id,taskname,taskdescription,status):
+        self.user_id=user_id
         self.taskname=taskname
         self.taskdescription=taskdescription
-        self.starttime=starttime
-        self.endtime=endtime
+        self.starttime=datetime.now()
+        self.endtime=None
         self.status=status
 
-    def enter_task(self,con):
-        cursor.execute("INSERT INTO TASK ( ID,TASKNAME,DESCRIPTION,STARTIME,ENDTIME,STATUS) VALUES(?,?,?,?,?)",
-                       (self.id,self.taskname,self.taskdescription,self.starttime,self.endtime,self.status))
-        con.commit()
-
+    def stop(self):
+        self.endtime=datetime.now()
     
+    def get_duration(self):
+        "gets the duration of task"
+        if self.endtime is None:
+            return None
+        else:
+            duration=self.endtime-self.starttime
+            return duration.total_seconds()
+        
+    def to_dict(self):
+        return {
+            "user_id":self.user_id,
+            "taskname":self.taskname,
+            "description":self.taskdescription,
+            "starttime":self.starttime.strftime("%Y-%m-%d %H:%M:%S"),
+            "entime":self.endtime.strftime("%Y-%m-%d %H:%M:%S") if self.endtime else None
+             }
+    
+class TaskManager:
+    def __init__(self,con):
+        self.con=con
+
+    def save_task(self,task):
+        cursor=self.con.cursor()
+        cursor.execute("INSERT INTO TASK(USER_ID,TASKNAME,DESCRIPTION,STARTTIME,ENDTIME,STATUS) VALUES(?,?,?,?,?,?)",
+                        (task.user_id,task.taskname,task.description,task.starttime.strftime("%Y-%m-%d %H:%M:%S"),task.endtime.strftime("%Y-%m-%d %H:%M:%S") if task.endtime else None,task.status))
+        
+        
+    def get_user_task(self,user_id):
+        cursor=self.con.cursor()
+        cursor.execute("SELECT id,taskname,description,starttime,endtime,status FROM TASK WHERE USER_ID=?", (user_id,))
+        rows=cursor.fetchall()
+        user_tasks=[]
+        for row in rows:
+            user_tasks.append(row)
+        return user_tasks
+
