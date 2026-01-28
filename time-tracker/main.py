a0d062f1926a02556c704f9bc89f3eeb879d9d40
faizan-abbas-mir <faixanmir21.fm@gmail.com>
2026-01-28 17:37:05 +0530

    feat:add tend task functionality, automatic id insertion of current user

--- time-tracker/main.py
+++ time-tracker/main.py
@@ -18,49 +18,64 @@ def signup():
 
 def login():
     entered_firstname=input("enter firstname: ")
-    id=input("enter your user id:\n")
     entered_password= input("enter password: ")
-    return entered_firstname,id,entered_password
+    return entered_firstname,entered_password
 
-user_input=input("do you have an account? y/n \n")
-if user_input=="y":
-    print("login function called")
-    id,entered_firstname,entered_password=login()
-    if manager.find_user(entered_firstname,entered_password):
-        print(f"welcome")
-    else:
-        print("no user found")
+login_success=False
+
+while login_success==False:
+    user_input=input("do you have an account? y/n \n")
+    if user_input=="y":
+        print("login function called")
+        entered_firstname,entered_password=login()
+        if manager.find_user(entered_firstname,entered_password):
+            loggedin_user=manager.find_user(entered_firstname,entered_password)
+            id=loggedin_user[0]
+            print(f"welcome")
+            print(id)
+            login_success=True
+        else:
+            print("no user found")
     
-elif user_input=="n":
-    print("signup function called")
-    firstname,lastname,password=signup()
-    user=User(firstname,lastname,password)
-    manager.save_user(user)
-    print(f"User saved! All users: {manager.list_all_users()}")
-    con.commit()
-    print("login function called")
-    entered_firstname,entered_password=login()
-    if manager.find_user(entered_firstname,entered_password):
-        print("welcome")
-    else:
-        print("no user found")
+    elif user_input=="n":
+        print("signup function called")
+        firstname,lastname,password=signup()
+        user=User(firstname,lastname,password)
+        manager.save_user(user)
+        print(f"User saved! All users: {manager.list_all_users()}")
+        con.commit()
+        print("login function called")
+        entered_firstname,entered_password=login()
+        if manager.find_user(entered_firstname,entered_password):
+            loggedin_user=manager.find_user(entered_firstname,entered_password)
+            id=loggedin_user[0]
+            print("welcome")
+            print(id)
+            login_success=True
+        else:
+            print("no user found")
 
 
 
-taskmanager.get_user_task(id)
+
 
 def new_task():
     taskname=input("what are you working on today?\n")
     print("sounds great... Lets add it to the task\n")
     description=input("enter the task description:\n")
     task=Task(id,taskname,description,status="stating")
+    input("enter stop to stop duration of task")
+    task.stop()
     taskmanager.save_task(task)
     con.commit()
-    print(f"task {taskname} started at {task.starttime}")
+    print(f"task {taskname} started at {task.starttime} and ended at {task.endtime}")
+
 activity=input("enter n for new task and h for history\n")
 if activity=="h":
     print(taskmanager.get_user_task(id))
 elif activity=="n":
     new_task()
 
-##import the id from login function and pass it to task function
+
+
+
